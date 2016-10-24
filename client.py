190a3515017464f73e57723d9844f35366dd1db2
Fernandoggs <FERNANDO@MacBook-Pro-de-Fernando-3.local>
2016-10-24 10:36:25 +0200

    Modificado cliente para que envie peticion SIP

--- client.py
+++ client.py
@@ -8,14 +8,18 @@ import socket
 import sys
 
 # Constantes. Dirección IP del servidor y contenido a enviar
-server = sys.argv[1]
+server_ip = sys.argv[1]
 port = int(sys.argv[2])
-line = ' '.join(sys.argv[3:])
+if not sys.argv[3] == 'register':
+    sys.exit("Usage: client.py ip puerto register sip_address")
+sip_address = sys.argv[4]
+#line = ' '.join(sys.argv[3:])
+line = 'REGISTER sip:' + sip_address + ' SIP/2.0\r\n'
 
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
-    my_socket.connect((server, port))
+    my_socket.connect((server_ip, port))
     print("Enviando:", line)
     my_socket.send(bytes(line, 'utf-8') + b'\r\n')
     data = my_socket.recv(1024)
